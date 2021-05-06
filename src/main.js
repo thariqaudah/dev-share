02a630c7b67345efc10d90023e96218d001f075f
Thariq Audah <thariq.audah@gmail.com>
2021-05-06 07:21:09 +0700

    Add CKEditor

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import CKEditor from 'ckeditor4-vue';
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 // Main styles
 import '@/assets/main.css';
 
 createApp(App)
-  .use(CKEditor)
   .use(router)
+  .use(CKEditor)
   .mount('#app');
--- src/views/articles/CreateArticle.vue
+++ src/views/articles/CreateArticle.vue
@@ -5,8 +5,11 @@
       <label for="title">Title</label>
       <input type="text" id="title" />
       <label for="content">Content</label>
-      <!-- <textarea name="content" id="content"></textarea> -->
-      <ckeditor></ckeditor>
+      <ckeditor
+        tagname="textarea"
+        :editor="editor"
+        v-model="editorData"
+      ></ckeditor>
       <label for="tags">Tags</label>
       <input type="text" id="tags" />
       <label for="photo">Photo</label>
@@ -16,7 +19,31 @@
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+export default {
+  setup() {
+    const editor = ClassicEditor;
+    const editorData = ref('');
+
+    return { editor, editorData };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+form {
+  max-width: 80%;
+  margin: auto;
+}
+form h1 {
+  border-bottom: 1px solid #eee;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+}
+form label {
+  display: inline-block;
+  margin-bottom: 10px;
+}
+</style>
